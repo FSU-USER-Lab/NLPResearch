cabed729ad103f9521cbc9c2434212987f829c63
haleydulay <43179228+haleydulay@users.noreply.github.com>
2020-04-02 12:27:22 -0400

    added some libraries...
    need to install nltk and nltk data to use them

--- nlp.py
+++ nlp.py
@@ -1,4 +1,10 @@
 import sys
+#need to make sure we install nltk and nltk data to use these functions:
+"""
+from nltk.corpus import remove_stopwords    #library for remove_stopwords function
+from nltk.stem import PorterStemmer as ps   #library for stem function
+"""
+
 """
 Currently working on Task 1 for the NLP project.
 goal is to preprocess the language in the datasets by taking out punctuation and stopwords and 
@@ -12,3 +18,5 @@ with open(sys.argv[1], 'r') as file:
     input = file.read()
 
 print(input)
+
+
