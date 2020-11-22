578055717ec6d2340935a1e1b8b053a265b620f9
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-11-21 19:31:04 -0500

    replaced top k method with get_vocabulary
    creates dictionary keyed by words found. value is a an integer id.

--- Project_2/utils.py
+++ Project_2/utils.py
@@ -1,24 +1,13 @@
-def build_vocabulary(str_list,  k=None):
-
-    language = {}
+def get_vocabulary(str_list):
+    wordID = 1
     vocabulary = {'UNK': 0, }
 
     for string in str_list:
         token_list = string.split()
         for token in token_list:
-            if token in language:
-                language[token] += 1
-            else:
-                language[token] = 1
-
-    #print({key: value for key, value in sorted(language.items(), key=lambda item: item[1])})
-    language = {key: value for key, value in sorted(
-        language.items(), key=lambda item: item[1], reverse=True)}
-    
-    if k is None:
-        vocabulary = vocabulary.update(dict(list(language.items())[0:]))
-    else:
-        vocabulary =  vocabulary.update(dict(list(language.items())[0:k]))
+            if token not in vocabulary.keys():
+                vocabulary[token] = wordID
+                wordID += 1
         
     return vocabulary
 
